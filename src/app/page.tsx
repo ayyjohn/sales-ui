1d93b1ec6c97e10e868365c38214510bf28c1327
Alec Johnson <alecjohnson55@gmail.com>
2025-01-28 10:03:01 -0800

    add luxon and clean up the chart to look more like the mockup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,33 +7,42 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
+import { DateTime } from "luxon";
 import Image from "next/image";
 // todo: mock this as a fake api call using react query
 import data from "public/data.json";
 import logo from "public/stackline_logo.svg";
-import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
+import { Line, LineChart, XAxis, YAxis } from "recharts";
 export default function HomePage() {
   const product = data[0];
   const sales = product?.sales;
   const chartData = sales?.map((sale) => ({
     weekEnding: sale.weekEnding,
+    // luxon helps so that 2017-01-01 is not automatically converted to Dec 2016 because of time zones
+    date: DateTime.fromISO(sale.weekEnding).toJSDate(),
     retailSales: sale.retailSales,
     wholesaleSales: sale.wholesaleSales,
+    retailerMargin: sale.retailerMargin,
   }));
   const chartConfig = {
+    retailSales: {
+      label: "Retail Sales",
+      color: "#45a7f6",
+    },
     wholesaleSales: {
       label: "Wholesale Sales",
       color: "#9ba6bf",
     },
-    retailSales: {
-      label: "Retail Sales",
-      color: "#45a7f6",
+    retailerMargin: {
+      label: "Retailer Margin",
+      color: "#9ba6bf",
     },
   } satisfies ChartConfig;
   return (
     <>
       <nav className="flex h-20 w-full items-center bg-[#052849]">
         <div className="ml-8 flex flex-col">
+          {/* todo: add svgr to avoid this error */}
           <Image src={logo} alt="Stackline Logo" height={30} />
           <div className="mt-2 h-1 w-full bg-white"></div>
         </div>
@@ -67,56 +76,64 @@ export default function HomePage() {
             </div>
           </div>
         </div>
-        <div className="col-span-7 row-span-6 rounded-sm bg-white shadow-lg">
-          <Card>
-            <CardHeader>
-              <CardTitle>Retail Sales</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <ChartContainer config={chartConfig}>
-                <LineChart
-                  accessibilityLayer
-                  data={chartData}
-                  margin={{
-                    left: 12,
-                    right: 12,
-                  }}
-                >
-                  <CartesianGrid vertical={false} />
-                  <XAxis
-                    dataKey="weekEnding"
-                    tickLine={false}
-                    axisLine={false}
-                    tickMargin={8}
-                    tickFormatter={(value: string) =>
-                      new Date(value).toLocaleDateString("en-US", {
+        <Card className="col-span-7 row-span-6 rounded-sm border-none bg-white shadow-lg">
+          <CardHeader>
+            <CardTitle>Retail Sales</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ChartContainer config={chartConfig}>
+              <LineChart
+                accessibilityLayer
+                data={chartData}
+                margin={{
+                  left: 12,
+                  right: 12,
+                }}
+              >
+                <YAxis
+                  dataKey="retailSales"
+                  tickLine={false}
+                  axisLine={false}
+                  tickMargin={8}
+                  hide={true}
+                  domain={["dataMin - 2000000", "dataMax + 2000000"]}
+                />
+                <XAxis
+                  dataKey="date"
+                  tickLine={false}
+                  axisLine={false}
+                  tickMargin={8}
+                  interval={4}
+                  tickFormatter={(value: string) =>
+                    new Date(value)
+                      .toLocaleDateString("en-US", {
                         month: "short",
                       })
-                    }
-                  />
-                  <ChartTooltip
-                    cursor={false}
-                    content={<ChartTooltipContent />}
-                  />
-                  <Line
-                    dataKey="retailSales"
-                    type="monotone"
-                    stroke={chartConfig.retailSales.color}
-                    strokeWidth={4}
-                    dot={false}
-                  />
-                  <Line
-                    dataKey="wholesaleSales"
-                    type="monotone"
-                    stroke={chartConfig.wholesaleSales.color}
-                    strokeWidth={4}
-                    dot={false}
-                  />
-                </LineChart>
-              </ChartContainer>
-            </CardContent>
-          </Card>
-        </div>
+                      .toUpperCase()
+                  }
+                />
+                <ChartTooltip
+                  cursor={false}
+                  content={<ChartTooltipContent />}
+                />
+                <Line
+                  dataKey="retailSales"
+                  type="monotone"
+                  stroke={chartConfig.retailSales.color}
+                  strokeWidth={4}
+                  dot={false}
+                />
+                <Line
+                  dataKey="wholesaleSales"
+                  type="monotone"
+                  stroke={chartConfig.wholesaleSales.color}
+                  strokeWidth={4}
+                  dot={false}
+                />
+              </LineChart>
+            </ChartContainer>
+          </CardContent>
+        </Card>
         <div className="col-span-7 row-span-6 rounded-sm bg-white shadow-lg">
           {/* todo: table */}
         </div>
