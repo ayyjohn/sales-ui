417f103974a6c022852b267ba261f8cc4c3b8d1e
Alec Johnson <alecjohnson55@gmail.com>
2025-01-28 13:56:53 -0800

    add loading skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import * as Card from "@/components/ui/card";
 import LineGraph from "@/components/ui/chart/line-graph";
 import { ProductDetail } from "@/components/ui/product/product-detail";
 import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
 import DataTable, { columns } from "@/components/ui/table/data-table";
 import { getProductData } from "@/server/fakeApi";
 import { useQuery } from "@tanstack/react-query";
@@ -17,36 +18,36 @@ export default function HomePage() {
   });
   const sales = product?.sales ?? [];
 
-  // todo: add skeleton loaders
-  if (isLoading) {
-    return (
-      <div className="flex h-full items-center justify-center">Loading...</div>
-    );
-  }
   return (
     <>
       <main className="grid grid-cols-1 gap-5 bg-[#f6f8fa] px-4 py-16 lg:grid-cols-[20rem_1fr] lg:grid-rows-[1fr_auto]">
-        <Card.Card className="col-span-1 row-span-2 rounded-sm border-none bg-white shadow-sm">
-          <Card.CardContent className="flex flex-col items-center p-4">
-            <ProductDetail product={product} />
-          </Card.CardContent>
-        </Card.Card>
-        <Card.Card className="col-span-1 row-span-1 rounded-sm border-none bg-white shadow-sm">
-          <Card.CardHeader>
-            <Card.CardTitle className="text-lg font-light">
-              Retail Sales
-            </Card.CardTitle>
-          </Card.CardHeader>
-          <Card.CardContent className="relative p-0">
-            <LineGraph sales={sales} />
-            <Separator className="absolute bottom-20 h-[1px] w-full bg-gray-100" />
-          </Card.CardContent>
-        </Card.Card>
-        <Card.Card className="col-span-1 row-span-1 rounded-sm border-none bg-white shadow-sm">
-          <Card.CardContent className="p-0">
-            <DataTable columns={columns} data={sales} />
-          </Card.CardContent>
-        </Card.Card>
+        <Skeleton loading={isLoading}>
+          <Card.Card className="col-span-1 row-span-2 rounded-sm border-none bg-white shadow-sm">
+            <Card.CardContent className="flex flex-col items-center p-4">
+              <ProductDetail product={product} />
+            </Card.CardContent>
+          </Card.Card>
+        </Skeleton>
+        <Skeleton loading={isLoading}>
+          <Card.Card className="rounded-sm border-none bg-white shadow-sm">
+            <Card.CardHeader>
+              <Card.CardTitle className="text-lg font-light">
+                Retail Sales
+              </Card.CardTitle>
+            </Card.CardHeader>
+            <Card.CardContent className="relative p-0">
+              <LineGraph sales={sales} />
+              <Separator className="absolute bottom-20 h-[1px] w-full bg-gray-100" />
+            </Card.CardContent>
+          </Card.Card>
+        </Skeleton>
+        <Skeleton loading={isLoading}>
+          <Card.Card className="rounded-sm border-none bg-white shadow-sm">
+            <Card.CardContent className="p-0">
+              <DataTable columns={columns} data={sales} />
+            </Card.CardContent>
+          </Card.Card>
+        </Skeleton>
       </main>
     </>
   );
---
+++ src/components/ui/skeleton.tsx
@@ -0,0 +1,46 @@
+import { cn } from "@/lib/utils";
+
+interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {
+  loading?: boolean;
+}
+
+// new version: wrap it around something the same size
+// as the thing you want it to replace and manipulate loading
+// and it'll size itself automatically
+function Skeleton({
+  className,
+  loading = true,
+  children,
+  ...props
+}: SkeletonProps) {
+  if (!loading) return children;
+  return (
+    <div className="relative inline-block">
+      <div
+        className={cn(
+          "absolute inset-0 animate-pulse rounded-md bg-primary/10",
+          className,
+        )}
+        {...props}
+      />
+      <div className="invisible">{children}</div>
+    </div>
+  );
+}
+
+export { Skeleton };
+
+// old version: you have to size it yourself
+// function Skeleton({
+//   className,
+//   ...props
+// }: React.HTMLAttributes<HTMLDivElement>) {
+//   return (
+//     <div
+//       className={cn("animate-pulse rounded-md bg-primary/10", className)}
+//       {...props}
+//     />
+//   )
+// }
+
+// export { Skeleton }
