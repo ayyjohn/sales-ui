36cf0da2bfcf5c357f1e6d98c8e9abd6828e24f4
Alec Johnson <alecjohnson55@gmail.com>
2025-01-28 14:08:20 -0800

    remove custom skeleton component

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,36 +1,5 @@
 import { cn } from "@/lib/utils";
 
-interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {
-  loading?: boolean;
-}
-
-// new version: wrap it around something the same size
-// as the thing you want it to replace and manipulate loading
-// and it'll size itself automatically
-// function Skeleton({
-//   className,
-//   loading = true,
-//   children,
-//   ...props
-// }: SkeletonProps) {
-//   if (!loading) return children;
-//   return (
-//     <div className="relative inline-block">
-//       <div
-//         className={cn(
-//           "absolute inset-0 animate-pulse rounded-md bg-primary/10",
-//           className,
-//         )}
-//         {...props}
-//       />
-//       <div className="invisible">{children}</div>
-//     </div>
-//   );
-// }
-
-// export { Skeleton };
-
-// old version: you have to size it yourself
 function Skeleton({
   className,
   ...props
