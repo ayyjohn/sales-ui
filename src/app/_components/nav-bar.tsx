36d77a6e0e0388d544d73731ece1f7c1b24276df
Alec Johnson <alecjohnson55@gmail.com>
2025-01-28 14:23:01 -0800

    change logo to an svg to avoid using svgr

--- src/app/_components/nav-bar.tsx
+++ src/app/_components/nav-bar.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
-import logo from "public/stackline_logo.svg";
+import StacklineLogo from "@/components/ui/logos/stackline-logo";
 
 export function NavBar() {
   return (
     <nav className="sticky top-0 z-10 flex h-20 w-full items-center bg-[#052849]">
       <div className="ml-8 flex flex-col">
-        <Image src={logo} alt="Stackline Logo" height={30} />
+        <StacklineLogo className="h-[30px]" />
         <div className="mt-2 h-1 w-full bg-white"></div>
       </div>
     </nav>
